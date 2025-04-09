70ba5acfae83bd85f3b3295f313019b6449fdd8b
Enrok <enrok.gaming@gmail.com>
2025-04-09 08:35:22 +0200

    Praca Domowa Urządzenia poprawione v2

--- UrzadzenieElektryczne.cpp
+++ UrzadzenieElektryczne.cpp
@@ -14,20 +14,20 @@ void UrzadzenieElektryczne::wylacz() {
     stan = false;
 }
 
-std::string UrzadzenieElektryczne::jakaMarka() const{
+std::string UrzadzenieElektryczne::jakaMarka() const {
     return marka;
 }
 
-bool UrzadzenieElektryczne::jakiStan() const{
+bool UrzadzenieElektryczne::jakiStan() const {
     return stan;
 }
 
-void UrzadzenieElektryczne::wypisz() const{
+void UrzadzenieElektryczne::wypisz() const {
     std::string marka = jakaMarka();
     if (jakiStan()) {
-        std::cout << marka << " jest wlaczony" << std::endl;
+        std::cout << marka << " jest wlaczony ";
     } else {
-        std::cout << marka << " jest wylaczony" << std::endl;
+        std::cout << marka << " jest wylaczony ";
     }
 }
 
@@ -35,33 +35,32 @@ TV::TV(std::string marka1, bool stan, int kanal1, int glosnosc1) : UrzadzenieEle
                                                                    glosnosc(glosnosc1) {
 }
 
-void TV::podglosnij(){
-    if (glosnosc < 10) {
+void TV::podglosnij() {
+    if (jakiStan() && glosnosc < 10) {
         glosnosc++;
     }
 }
 
-void TV::scisz(){
-    if (glosnosc > 0) {
+void TV::scisz() {
+    if (jakiStan() && glosnosc > 0) {
         glosnosc--;
     }
 }
 
-void TV::zmienKanal(int kanal1){
+void TV::zmienKanal(int kanal1) {
     if (kanal1 <= 40 && kanal1 >= 1) {
         kanal = kanal1;
     } else {
-        std::cout << "Nie ma takiego kanału"<<std::endl;
+        std::cout << "Nie ma takiego kanalu" << std::endl;
     }
     if (!jakiStan()) {
         stan = true;
     }
 }
 
-void TV::wypisz() const{
+void TV::wypisz() const {
+    UrzadzenieElektryczne::wypisz();
     if (stan) {
-        std::cout << marka << " jest wlaczony na kanale " << kanal << "(glosnosc " << glosnosc << ")" << std::endl;
-    } else {
-        std::cout << marka << " jest wylaczony" << std::endl;
+        std::cout << "na kanale " << kanal << "(glosnosc " << glosnosc << ")" << std::endl;
     }
 }
